Obvious errors with average hnu for multiply excited states corrected. Some bugs remain in plotting, e.g. indexing the correct states. But the averages seem to be correct.

FAC/fac_write_configs.py
@@ -154,35 +154,42 @@ def generate_complexes(NE, nmax, exc=1, fac_readable=True):
         clo = np.zeros(nmax).astype(int) # clo = core-low
         clo[:2] =[2,8-exc]
         
-        if exc==0: # If ground state, and therefore n=2 is full, there is no upper state            
-            # Wrap in a list to enable iteration later           
-            lo = [clo]
-            up = [[]]
+        # if exc==0: # If ground state, and therefore n=2 is full, there is no upper state            
+        #     # Wrap in a list to enable iteration later           
+        #     lo = [clo]
+        #     up = [[]]
             
-        else: # If excited, and therefore n=2 is not full, construct upper state
-            cup = clo * 1 # Copy lower populations
-            
-            # Promote n=1 electron to n=2
-            cup[0] -= 1 
-            cup[1] += 1
+        # else: # If excited, and therefore n=2 is not full, construct upper state
+        cup = clo * 1 # Copy lower populations
+        
+        # Promote n=1 electron to n=2
+        cup[0] -= 1 
+        cup[1] += 1
 
-            Nexc = NE - (10-exc) # Number of electrons in n>2 shells
+        Nexc = NE - (10-exc) # Number of electrons in n>2 shells
+        
+        # Generator object. All ways of assigning Nexc electrons to shells n>2
+        if Nexc>0:
+            combinations = all_counts(Nexc, nmax-2)
+        else:
+            combinations = [[0,0,0]] # Only occurs for ground-state Ne-like
+        
+        lo, up = [], [] # Initiate lists
+        for c in combinations: # Append each upper shell population to up and lo
+            # Copy core configuration
+            ltmp = clo * 1
+            utmp = cup * 1
             
-            # Generator object. All ways of assigning Nexc electrons to shells n>2
-            combinations = all_counts(Nexc, nmax-2) 
+            # Append upper shell populations
+            ltmp[2:] = c
+            utmp[2:] = c
             
-            lo, up = [], [] # Initiate lists
-            for c in combinations: # Append each upper shell population to up and lo
-                # Copy core configuration
-                ltmp = clo * 1
-                utmp = cup * 1
-                
-                # Append upper shell populations
-                ltmp[2:] = c
-                utmp[2:] = c
-                
-                lo.append(ltmp)
-                up.append(utmp)
+            lo.append(ltmp)
+            up.append(utmp)
+            
+        if exc==0:
+            # If ground state, and therefore n=2 is full, there is no upper state   
+            up = ['']*len(up) # Replace upper states with an equal number of empty strings
 
     if not(fac_readable):
         return [lo, up]

ScHyd_AtomicData.py
@@ -151,6 +151,7 @@ class AtDat():
                             pops = lo
                     
                     for Pni in pops:
+                        
                         if len(Pni)==0:
                             # Append NaN to each, to keep same length as corresponding up/lo
                             Enx.append(np.nan)
@@ -221,13 +222,22 @@ class AtDat():
         
 
         # Initialize output dictionaries
-        self.max_length = {}
         self.Earrs = {}
         self.excarrs = {}
         self.gtot_lists = {}
         self.hnuarrs = []
         self.Pnarrs = {}
-        
+
+        # Find max_length of all arrays first
+        lengths = []
+        for uplo in ['lo', 'up']:
+            for Z in self.Zkeys:
+                tmp = []
+                [tmp.extend(self.Etot[uplo][Z][str(e)])
+                     for e in list(self.Etot[uplo][Z].keys())]
+                lengths.append(len(tmp))
+        max_length = max(lengths)
+        self.max_length = max_length # Longest length array, i.e. maximum number of complexes for a given charge state
 
         # Zs = [Zkey for Zkey in list(Etot['lo'].keys()) if list(Etot['up'][Zkey].keys())] # Keep only calculated charge states
         for uplo in ['lo', 'up']:
@@ -264,8 +274,6 @@ class AtDat():
             # To enable slicing, 0-pad all state-resolved arrays. Shape: [Number charge states, max_length]
             # 0 values in gtot_lists reslut in 0 contribution to partition functions, 0 effect on Saha-Boltzmann
             # Pad no zeroes at beginning, out to max_length at end: pad_width = [(0, max_length-len(item))]
-            max_length = max([len(item) for item in excarrs]) # Longest length array, i.e. maximum number of complexes for a given charge state
-            self.max_length[uplo] = max_length
             self.Earrs[uplo]      = np.array([np.pad(item, [(0, max_length-len(item))], constant_values=int(0)) for item in Earrs]) 
             self.excarrs[uplo]    = np.array([np.pad(item, [(0, max_length-len(item))], constant_values=int(-1)) for item in excarrs]) # Pad -1 to avoid confusion with 0
             self.hnuarrs    = np.array([np.pad(item, [(0, max_length-len(item))], constant_values=int(0)) for item in hnuarrs]) # Doesn't need to be a dictionary
@@ -275,6 +283,14 @@ class AtDat():
             self.gtot_lists[uplo] = np.array([np.pad(item, [(0, max_length-len(item)), (0,0)], constant_values=int(0)) for item in glists]) 
                 # gtot = total statistical weight of each configuration, of each shell, even those not available for transition.
                 # Atom's total statweight = np.prod(self.gtot_lists['lo'], axis=-1)
+                
+            # Define satellite array according to the core (n=1,2) population
+            self.satarrs = self.Pnarrs['lo'][:,:,:2].sum(axis=-1) # Sum over inner population
+            
+            # Get list of unique satellites
+            sats = np.unique(self.satarrs)[-1::-1] # Sats in decreasing value, least-ionized first
+            self.sats = sats[sats>0] # Remove unassigned=0
+
 
         return
     
@@ -318,7 +334,7 @@ class AtDat():
 
         Zbar = np.zeros(shape=[self.NT,self.Nn]) # Mean ionization state
         psaha = np.zeros(shape=[self.NT,self.Nn,len(self.Zkeys)+1]) # Saha charge state populations. Shape T, Ne, Z+1
-        pboltz = np.zeros(shape=[self.NT,self.Nn,len(self.Zkeys), self.max_length['lo']]) # Boltzmann state populations. Shape T, Ne, Z, state
+        pboltz = np.zeros(shape=[self.NT,self.Nn,len(self.Zkeys), self.max_length]) # Boltzmann state populations. Shape T, Ne, Z, state
         for idx, items in enumerate(it.product(KT,NE)):
             kT, ne = items
             i, j = np.unravel_index(idx, shape=(self.NT,self.Nn))
@@ -368,7 +384,7 @@ class AtDat():
         # Run Boltzmann on each charge state over rho grid
         g = np.prod(self.gtot_lists['lo'], axis=-1)
 
-        pboltz_rho = np.zeros(shape=[self.NT,self.Nrho,len(self.Zkeys), self.max_length['lo']]) # Shape T, Ne, Z, state
+        pboltz_rho = np.zeros(shape=[self.NT,self.Nrho,len(self.Zkeys), self.max_length]) # Shape T, Ne, Z, state
         for idx, items in enumerate(it.product(self.KT,rho_grid)):
             kT, __ = items
             i, j = np.unravel_index(idx, shape=(self.NT,self.Nrho))
@@ -658,6 +674,17 @@ class AtDat():
             for exc in self.exc_list:
                 cond = self.excarrs[uplo]==exc
                 
+                # Troubleshoot correspondence between cond and hnu
+                # z=0
+                # print(exc)
+                # print(cond[z])
+                # print(self.Pnarrs['lo'][z])
+                # print(self.Pnarrs['up'][z])
+                
+                # print(self.hnuarrs[z])
+                # print(self.hnuarrs[z,cond[z]])
+                
+                
                 # Sum over allowed conditions, once for each ionization state
                 tmp_hnu = np.array([np.sum((self.hnuarrs*pops*gf)[Ellipsis,i,c], axis=-1) \
                                 / np.sum( (pops*gf)[Ellipsis,i,c], axis=-1)
@@ -678,7 +705,7 @@ class AtDat():
                 #     or np.any(np.isnan([self.hnuarrs[i,c] for i,c in enumerate(cond)])) \
                 #     or np.any(np.isnan([gf[i,c] for i,c in enumerate(cond)])):
                 #     breakpoint()
-
+                
                 hnu_avg.append(tmp_hnu)
                 pgf.append(tmp_pgf)
                 
@@ -687,34 +714,26 @@ class AtDat():
             
         elif resolve=='line':
             # Return line-complex resolved line centers
-            for zidx in range(pops.shape[2]):
-                tmp_pgf = np.zeros([*pops.shape[:2],0]) # Populations x weighted osc.str.
-                tmp_hnu = [] # Transition energies within satellite 
-                for exc in self.exc_list:
-                    if (zidx-exc)>=0:
-                        # Charge state index = zidx-exc to group within line-complex
-                        cond = self.excarrs[uplo][zidx-exc]==exc                    
-                        tmp_hnu.extend(self.hnuarrs[zidx-exc,cond]) # Lower ionization, higher excitation
-                        tmp_pgf = np.dstack([tmp_pgf, (pops*gf)[:,:,zidx-exc,cond]])
-
-                        # Troubleshoot if NaNs or zeros are in the hnu sum/average
-                        # breakpoint()
-                        # print(self.hnuarrs[zidx-exc,cond])
-                        # print(gf[zidx-exc,cond])
+            tmp_pgf = np.zeros([*pops.shape[:2],0]) # Populations x weighted osc.str.
+            tmp_hnu = [] # Transition energies within satellite 
                         
-                # Sum over allowed conditions, once for each ionization state
-                hnu_avg.append(np.sum(tmp_hnu*tmp_pgf, axis=-1) \
-                               / np.sum(tmp_pgf, axis=-1)) # Shape [ionization, NT, Nrho]
-                pgf.append(np.sum(tmp_pgf, axis=-1))
+            # Iterate over each satellite complex
+            for s in self.sats:
+                cond = self.satarrs==s # 2-D condition, across ionization and excitation
+                
+                # Sum over allowed conditions, over all corresponding excitation and ionization states
+                # cond is non-regular over ionization and excitation, so only sum(axis=-1)
+                tmp_hnu = (self.hnuarrs*pops*gf)[Ellipsis,cond].sum(axis=-1) \
+                                / (pops*gf)[Ellipsis,cond].sum(axis=-1) # Shape [NT, Nrho]
 
-                # if np.any([self.hnuarrs[i,c]==0 for i,c in enumerate(cond)]) \
-                #     or np.any([gf[i,c]==0 for i,c in enumerate(cond)]) \
-                #     or np.any(np.isnan([self.hnuarrs[i,c] for i,c in enumerate(cond)])) \
-                #     or np.any(np.isnan([gf[i,c] for i,c in enumerate(cond)])):
-                #     breakpoint()
-
+                tmp_pgf = (pops*gf)[Ellipsis,cond].sum(axis=-1) # Shape [NT, Nrho]
+                
+                hnu_avg.append(tmp_hnu) # Shape [ionization, NT, Nrho]
+                pgf.append(tmp_pgf)     # Shape [ionization, NT, Nrho]
+                    
             hnu_avg = np.moveaxis(hnu_avg, [0,1,2], [2,0,1]) # Shape [NT, Nrho, ionization]
             pgf = np.moveaxis(pgf, [0,1,2], [2,0,1]) # Shape [NT, Nrho, ionization]
+                   
         if return_weight:
             return hnu_avg, pgf
             

examples/hnu_average.py
@@ -32,8 +32,8 @@ A = 51.996 # Nucleon number
 
 Zbar_min = ZZ - 15
 nmax = 5 # Maximum allowed shell
-exc_list = [0,1,2,] # Excitation degrees to consider (lower state is ground state, singly excited, ...)
-# exc_list = [0,1] # Excitation degrees to consider (lower state is ground state, singly excited, ...)
+exc_list = np.arange(6) # Excitation degrees to consider (lower state is ground state, singly excited, ...)
+# exc_list = [0,1,2] # Excitation degrees to consider (lower state is ground state, singly excited, ...)
 pf = 1
 
 # Run model
@@ -68,7 +68,7 @@ Nrho = 12
 rho_grid = np.logspace(-1,3, num=Nrho)
 
 # Estimate IPD for Saha and interpolate onto NE vector
-if 1: # Skip while testing
+if 0: # Skip while testing
     Zgrid_rho, __, __, __, __, __, CLgrid_rho = dense_plasma(Z=ZZ, Zbar=1, A=A, Ts=KT, rhos=rho_grid,
                                                               CL='IS Atzeni')
     
@@ -109,7 +109,7 @@ if 0:
 
 # %% Populations and gf
 # Run Saha-Boltzmann
-ad.saha_boltzmann(KT, NE, IPD=CLgrid) # NE-indexed
+ad.saha_boltzmann(KT, NE, IPD=0) # NE-indexed
 ad.saha_boltzmann_rho(rho_grid) # rho_grid-indexed
 
 # View Zbar from Saha-Boltzmann
@@ -145,12 +145,13 @@ gf = ad.get_gf(1,0,2,1, return_gs=False)
 
 #### Average hnu
 # Keep indexed to rho_grid throughout
-# Satellite-resolved
-sat_avg, pgf_sat = ad.get_hnu_average(ad.pstate_rho, gf=gf, resolve='ionization',
+# Averaged within an excitation degree
+hnu_exc, pgf_exc = ad.get_hnu_average(ad.pstate_rho, gf=gf, resolve='ionization',
                              return_weight=True) # Shape: [excitation, NT, Nrho, ionization]
-# Line-complex resolved line centers
-hnu_avg, pgf = ad.get_hnu_average(ad.pstate_rho, gf=gf, resolve='line',
-                                  return_weight=True) # Shape: [excitation, NT, Nrho, ionization]
+# Average within a satellite complex
+# breakpoint()
+hnu_sat, pgf = ad.get_hnu_average(ad.pstate_rho, gf=gf, resolve='line',
+                                  return_weight=True) # Shape: [NT, Nrho, satellite]
 
 # %% Opacity
 if 0:
@@ -195,8 +196,8 @@ print('{0:0.2f} eV, {1:0.2f} g/cc'.format(KT[Tidx], rho_grid[rhoidx]))
 print('{0:5s} | {1:3s} | {2:10s} | {3:10s}'.format('Zbar','exc', 'pop x gf', 'pop x gf (norm)'))
 [print('{0:5s} | {1:3d} | {2:10.1e} | {3:10.1e}'.format(
     ad.Zkeys[zidx], e,
-    pgf_sat[e,Tidx,rhoidx,zidx],
-    pgf_sat[e,Tidx,rhoidx,zidx]/pgf_sat[0,Tidx,rhoidx,zidx])) for e in exc_list]
+    pgf_exc[e,Tidx,rhoidx,zidx],
+    pgf_exc[e,Tidx,rhoidx,zidx]/pgf_exc[0,Tidx,rhoidx,zidx])) for e in exc_list]
 
 print('Saha balance')
 [print('{0:s} : {1:8.1e}'.format(ad.Zkeys[zidx], ad.psaha[Tidx,rhoidx,zidx])) for zidx in range(ad.psaha.shape[-1]-1)];
@@ -216,11 +217,15 @@ for e in exc_list:
 
 # %% Plot: Excitation-resolved lines
 rhoidx = 7
-zidx = len(ad.Zkeys)-5 # 3 = N-like
+
+# Define satellite to zoom in on
+sat = 6 # Isoelectronic
+sidx = np.where(ad.sats==sat)[0][0] # Satellite index
+zidx = np.where(np.isin(ad.Zkeys, '{0:0.0f}'.format(ad.Z-sat)))[0][0] # Corresponding ionization index
 
 # Single charge-state
 plt.figure(figsize=[4,3])
-[plt.semilogx(KT, sat_avg[eidx,:,rhoidx,zidx-eidx],
+[plt.semilogx(KT, hnu_exc[eidx,:,rhoidx,zidx-eidx],
           color='C{0:d}'.format(eidx),
           label='Z*={0:s}, exc={1:d} new'.format(ad.Zkeys[zidx-eidx], eidx))
      for eidx in exc_list if (zidx-eidx)>=0]
@@ -230,7 +235,7 @@ plt.legend()
 
 # All charge states
 plt.figure(figsize=[4,3])
-[plt.semilogx(KT, sat_avg[eidx,:,rhoidx,:],
+[plt.semilogx(KT, hnu_exc[eidx,:,rhoidx,:],
           color='C{0:d}'.format(eidx),
           # label='Z*={0:s}, exc={1:d} new'.format(ad.Zkeys[zidx-eidx], eidx)
           )
@@ -251,13 +256,13 @@ axs[0].set(ylabel='Zbar',
            title=r'$\rho$={0:0.1e} g/cm$^3$'.format(rho_grid[rhoidx]))
 
 # Satellite resolved line centers
-[axs[1].plot(KT, sat_avg[eidx,:,rhoidx,zidx-eidx],
+[axs[1].plot(KT, hnu_exc[eidx,:,rhoidx,zidx-eidx],
           color='C{0:d}'.format(eidx),
           label='Z*={0:s}, exc={1:d}'.format(ad.Zkeys[zidx-eidx], eidx))
      for eidx in exc_list if (zidx-eidx)>=0]
 
 # Line-complex resolved line centers
-axs[1].plot(KT, hnu_avg[:,rhoidx,zidx], label='Averaged', color='k')
+axs[1].plot(KT, hnu_sat[:,rhoidx,sidx], label='Averaged', color='k')
 
 axs[1].set(xlabel='kT (eV)',
           ylabel='hnu (eV)')
@@ -266,7 +271,7 @@ plt.legend(bbox_to_anchor=(1.,1))
 
 #### All satellite complexes, average only
 # plt.figure(figsize=[4,3])
-# plt.semilogx(KT, hnu_avg[:,rhoidx,:], label=ad.Zkeys)
+# plt.semilogx(KT, hnu_sat[:,rhoidx,:], label=ad.Zkeys)
 # plt.gca().set(ylim=[5350,5800],
 #               xlabel='kT (eV)',
 #               ylabel='hnu (eV)')
@@ -281,16 +286,16 @@ axs[0].set(ylabel='Zbar',
                                                                         ad.Zkeys[zidx]))
 
 
-[axs[1].plot(KT, sat_avg[eidx,:,rhoidx,zidx-eidx],
+[axs[1].plot(KT, hnu_exc[eidx,:,rhoidx,zidx-eidx],
           color='C{0:d}'.format(eidx),
           label='Z*={0:s}, exc={1:d}'.format(ad.Zkeys[zidx-eidx], eidx))
      for eidx in exc_list if (zidx-eidx)>=0]
 
 # Line-complex resolved line centers
-axs[1].scatter(KT, hnu_avg[:,rhoidx,zidx], label='Averaged', color='k',
+axs[1].scatter(KT, hnu_sat[:,rhoidx,sidx], label='Averaged', color='k',
             facecolor='None')
-axs[1].scatter(KT, hnu_avg[:,rhoidx,zidx], color='k',
-            alpha=pgf[:,rhoidx,zidx]/np.nanmax(pgf[:,rhoidx,zidx]))
+axs[1].scatter(KT, hnu_sat[:,rhoidx,sidx], color='k',
+            alpha=pgf[:,rhoidx,sidx]/np.nanmax(pgf[:,rhoidx,sidx]))
 
 axs[1].set(xlabel='kT (eV)',
           ylabel='hnu (eV)',
@@ -301,26 +306,36 @@ axs[1].set(xlabel='kT (eV)',
 
 axs[1].legend(bbox_to_anchor=(1.,0.6))
 
-# # All complexes
-# fig, axs = plt.subplots(len(ad.Zkeys)-1, figsize=[8,24], sharex=True)
-# zmin = 1
-# for zzz in range(zmin,len(ad.Zkeys)):
-#     [axs[zzz-zmin].plot(KT, sat_avg[eidx,:,rhoidx,zzz-eidx],
-#               color='C{0:d}'.format(eidx),
-#               label='Z*={0:s}, exc={1:d}'.format(ad.Zkeys[zzz-eidx], eidx))
-#          for eidx in exc_list if (zzz-eidx)>=0]
+# Each satellite
+smin = 1
+fig, axs = plt.subplots(len(ad.sats)-smin, figsize=[8,24], sharex=True)
+for axi,sss in enumerate(ad.sats[smin:][-1::-1]):
+    si = np.where(ad.sats==sss)[0][0]
+    zi = np.where(np.isin(ad.Zkeys, '{0:0.0f}'.format(ad.Z-sss)))[0][0]
+    cond = np.where(ad.satarrs==sss)
+    # for i,e in zip(*cond):
+    #     axs[axi].plot(KT, ad.hnuarrs[i,e], color='C{0:0.0f}'.format(ad.excarrs['lo'][i,e]))
+    [axs[axi].plot(KT, hnu_exc[eidx,:,rhoidx,:],
+              color='C{0:d}'.format(eidx),
+              )
+          for eidx in exc_list]
     
-#     # Line-complex resolved line centers
-#     axs[zzz-zmin].scatter(KT, hnu_avg[:,rhoidx,zzz], label='Averaged', color='k',
-#                 facecolor='None')
-#     axs[zzz-zmin].scatter(KT, hnu_avg[:,rhoidx,zzz], color='k',
-#                 alpha=pgf[:,rhoidx,zzz]/np.nanmax(pgf[:,rhoidx,zzz]))
+    # Line-complex resolved line centers
+    axs[axi].scatter(KT, hnu_sat[:,rhoidx,si], label='Averaged', color='k',
+                facecolor='None')
+    axs[axi].scatter(KT, hnu_sat[:,rhoidx,si], color='k',
+                alpha=pgf[:,rhoidx,si]/np.nanmax(pgf[:,rhoidx,si]))
     
-# axs[zzz-zmin].set(xlabel='kT (eV)',
-#           ylabel='hnu (eV)',
-#           # xscale='log',
-#             xlim=[0,1100],
-#           )
+    axs[axi].set(ylim= [np.nanmin(hnu_sat[:,rhoidx,si]),
+                        # np.nanmax(hnu_sat[:,rhoidx,si])
+                        hnu_exc[0,-1,-1,zi] + 1
+                        ])
+    
+axs[-1].set(xlabel='kT (eV)',
+          ylabel='hnu (eV)',
+          # xscale='log',
+            xlim=[0,1100],
+          )
 
 
 